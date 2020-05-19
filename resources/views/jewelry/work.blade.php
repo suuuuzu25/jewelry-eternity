6743e7fffebf4fbbfdb785d4ea04fdfbca6fa89f
おしゅたん <suzu@Airtan.local>
2020-05-19 14:25:21 +0900

    first commit

--- resources/views/jewelry/work.blade.php
+++ resources/views/jewelry/work.blade.php
@@ -8,38 +8,56 @@
                 <p>作品</p>
             </div>
             <div class="work-container">
-                    <div class="row">
-                        <div class="headline col-md-10 mx-auto">
-                            <div class="row">
-                                <div class="work-image　mx-auto">
-                                    <img src="img\IMG_6412.jpg" width="228" height="261">
+                <div class="row">
+                    <div class="headline col-md-10 mx-auto">
+                        <div class="row">
+                            <div class="work-image　mx-auto">
+                                <img src="img\IMG_5190.JPG"　width="437" height="328" alt="写真">
+                            </div>
+                            <div class="work-block　mx-auto">
+                                <div class="work-title">
+                                    <p>ここはタイトルです</p>
                                 </div>
-                                <div class="work-block　mx-auto">
-                                    <div class="work-title">
-                                        <p>ここはタイトルです</p>
-                                    </div>
-                                    <div class="description">
-                                        <p>ここは説明文ですここは説明文ですここは説明文ですここは説明文です</p>
-                                    </div>
+                                <div class="description">
+                                    <p>ここは説明文ですここは説明文ですここは説明文ですここは説明文です</p>
                                 </div>
                             </div>
                         </div>
+                    </div>
                 </div>
             </div>
             <div class="work-container">
-                    <div class="row">
-                        <div class="headline col-md-10 mx-auto">
-                            <div class="row">
-                                <div class="work-image　mx-auto">
-                                    <img src="img\IMG_6412.jpg" width="228" height="261">
+                <div class="row">
+                    <div class="headline col-md-10 mx-auto">
+                        <div class="row">
+                            <div class="work-image　mx-auto">
+                                <img src="img\IMG_6412.jpg" width="286" height="328">
+                            </div>
+                            <div class="work-block　mx-auto">
+                                <div class="work-title">
+                                    <p>ここはタイトルです</p>
                                 </div>
-                                <div class="work-block　mx-auto">
-                                    <div class="work-title">
-                                        <p>ここはタイトルです</p>
-                                    </div>
-                                    <div class="description">
-                                        <p>ここは説明文ですここは説明文ですここは説明文ですここは説明文です</p>
-                                    </div>
+                                <div class="description">
+                                    <p>ここは説明文ですここは説明文ですここは説明文ですここは説明文です</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="work-container">
+                <div class="row">
+                    <div class="headline col-md-10 mx-auto">
+                        <div class="row">
+                            <div class="work-image　mx-auto">
+                                <img src="img\IMG_3284.jpg"　width="278" height="328" alt="写真">
+                            </div>
+                            <div class="work-block　mx-auto">
+                                <div class="work-title">
+                                    <p>ここはタイトルです</p>
+                                </div>
+                                <div class="description">
+                                    <p>ここは説明文ですここは説明文ですここは説明文ですここは説明文です</p>
                                 </div>
                             </div>
                         </div>
@@ -47,8 +65,6 @@
                 </div>
             </div>
         </div>
-
-
     </div>
 </div>
 @endsection
