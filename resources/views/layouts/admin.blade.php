c2e1c6a1088f3587e0d0ac44a2776f980932bc90
おしゅたん <suzu@Airtan.local>
2020-05-20 14:20:36 +0900

    first commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -22,9 +22,9 @@
 
         <!-- Styles -->
         {{-- Laravel標準で用意されているCSSを読み込みます --}}
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
         {{-- この章の後半で作成するCSSを読み込みます --}}
-        <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
+        <link href="{{ mix('css/admin.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
