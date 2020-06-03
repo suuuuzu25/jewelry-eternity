5d670d7cbd13caeada2afb65454f2c03a3e4ffc1
おしゅたん <suzu@Airtan.local>
2020-06-04 00:08:41 +0900

    firstcommit

--- resources/views/jewelry/work.blade.php
+++ resources/views/jewelry/work.blade.php
@@ -10,6 +10,7 @@
             <div class="title">
                 <p>作品</p>
             </div>
+            <!-- 一つ目 -->
             <div class="work-container">
                 <div class="row">
                     <div class="headline col-md-10 mx-auto">
@@ -22,19 +23,20 @@
                                     <p>ここはタイトルです</p>
                                 </div>
                                 <div class="description">
-                                    <p>ここは説明文ですここは説明文ですここは説明文ですここは説明文です</p>
+                                    <p>ここは説明文ですここは説明文です</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <!-- 二つ目 -->
             <div class="work-container">
                 <div class="row">
                     <div class="headline col-md-10 mx-auto">
                         <div class="row">
                             <div class="work-image　mx-auto">
-                                <img src="img\IMG_6412.jpg" width="286" height="328">
+                                <img src="img\IMG_6412.jpg" width="286" height="328" alt="写真">
                             </div>
                             <div class="work-block　mx-auto">
                                 <div class="work-title">
@@ -48,6 +50,7 @@
                     </div>
                 </div>
             </div>
+            <!-- 三つ目 -->
             <div class="work-container">
                 <div class="row">
                     <div class="headline col-md-10 mx-auto">
@@ -61,6 +64,167 @@
                                 </div>
                                 <div class="description">
                                     <p>ここは説明文ですここは説明文ですここは説明文ですここは説明文です</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- 四つ目 -->
+            <div class="work-container">
+                <div class="row">
+                    <div class="headline col-md-10 mx-auto">
+                        <div class="row">
+                            <div class="work-image">
+                                 <img src="img\IMG_3273 2.JPG" width="320" height="320" alt="写真">
+                                 <img src="img\IMG_7805.jpg" width="320" height="296" alt="写真">
+                            </div>
+                            <div class="work-block　mx-auto">
+                                <div class="work-title">
+                                    <p>ここはタイトルです</p>
+                                </div>
+                                <div class="description">
+                                    <p>ここは説明文ですここは説明文ですここは説明文ですここは説明文です</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- 五つ目 -->
+            <div class="work-container">
+                <div class="row">
+                    <div class="headline col-md-10 mx-auto">
+                        <div class="row">
+                            <div class="work-image">
+                                 <img src="img\IMG_3342 2.JPG" width="320" height="356" alt="写真">
+                            </div>
+                            <div class="work-block　mx-auto">
+                                <div class="work-title">
+                                    <p>ここはタイトルです</p>
+                                </div>
+                                <div class="description">
+                                    <p>ここは説明文ですここは説明文ですここは説明文です</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- 六つ目 -->
+            <div class="work-container">
+                <div class="row">
+                    <div class="headline col-md-10 mx-auto">
+                        <div class="row">
+                            <div class="work-image">
+                                 <img src="img\IMG_3344 2.JPG" width="416" height="320" alt="写真">
+                            </div>
+                            <div class="work-block　mx-auto">
+                                <div class="work-title">
+                                    <p>ここはタイトルです</p>
+                                </div>
+                                <div class="description">
+                                    <p>ここは説明文ですここは説明文です</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- 七つ目 -->
+            <div class="work-container">
+                <div class="row">
+                    <div class="headline col-md-10 mx-auto">
+                        <div class="row">
+                            <div class="work-image">
+                                 <img src="img\IMG_3346.JPG" width="320" height="356" alt="写真">
+                            </div>
+                            <div class="work-block　mx-auto">
+                                <div class="work-title">
+                                    <p>ここはタイトルです</p>
+                                </div>
+                                <div class="description">
+                                    <p>ここは説明文ですここは説明文です</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- 八つ目 -->
+            <div class="work-container">
+                <div class="row">
+                    <div class="headline col-md-10 mx-auto">
+                        <div class="row">
+                            <div class="work-image">
+                                <img src="img\IMG_4480.jpg" width="424" height="320" alt="写真">
+                            </div>
+                            <div class="work-block　mx-auto">
+                                <div class="work-title">
+                                    <p>ここはタイトルです</p>
+                                </div>
+                                <div class="description">
+                                    <p>ここは説明文ですここは説明文です</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- 九つ目 -->
+            <div class="work-container">
+                <div class="row">
+                    <div class="headline col-md-10 mx-auto">
+                        <div class="row">
+                            <div class="work-image">
+                                <img src="img\IMG_6394.jpg" width="439" height="320" alt="写真">
+                            </div>
+                            <div class="work-block　mx-auto">
+                                <div class="work-title">
+                                    <p>ここはタイトルです</p>
+                                </div>
+                                <div class="description">
+                                    <p>ここは説明文ですここは説明文です</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- 十個目 -->
+            <div class="work-container">
+                <div class="row">
+                    <div class="headline col-md-10 mx-auto">
+                        <div class="row">
+                            <div class="work-image">
+                                <img src="img\IMG_8122.jpg" width="427" height="320" alt="写真">
+                            </div>
+                            <div class="work-block　mx-auto">
+                                <div class="work-title">
+                                    <p>ここはタイトルです</p>
+                                </div>
+                                <div class="description">
+                                    <p>ここは説明文ですここは説明文です</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- 十一個目 -->
+            <div class="work-container">
+                <div class="row">
+                    <div class="headline col-md-10 mx-auto">
+                        <div class="row">
+                            <div class="work-image">
+                                <img src="img\IMG_9790.JPG" width="390" height="320" alt="写真">
+                            </div>
+                            <div class="work-block　mx-auto">
+                                <div class="work-title">
+                                    <p>ここはタイトルです</p>
+                                </div>
+                                <div class="description">
+                                    <p>ここは説明文ですここは説明文です</p>
                                 </div>
                             </div>
                         </div>
