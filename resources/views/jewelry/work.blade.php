50212830e033c3e480a8b5bef83e1d21a6204945
おしゅたん <suzu@Airtan.local>
2020-06-04 00:16:46 +0900

    firstcommit

--- resources/views/jewelry/work.blade.php
+++ resources/views/jewelry/work.blade.php
@@ -16,7 +16,7 @@
                     <div class="headline col-md-10 mx-auto">
                         <div class="row">
                             <div class="work-image　mx-auto">
-                                <img src="img\IMG_5190.JPG"　width="437" height="328" alt="写真">
+                                <img src="img\IMG_5190.JPG" 　width="437" height="328" alt="写真">
                             </div>
                             <div class="work-block　mx-auto">
                                 <div class="work-title">
@@ -56,7 +56,7 @@
                     <div class="headline col-md-10 mx-auto">
                         <div class="row">
                             <div class="work-image　mx-auto">
-                                <img src="img\IMG_3284.jpg"　width="278" height="328" alt="写真">
+                                <img src="img\IMG_3284.jpg" 　width="278" height="328" alt="写真">
                             </div>
                             <div class="work-block　mx-auto">
                                 <div class="work-title">
@@ -75,9 +75,9 @@
                 <div class="row">
                     <div class="headline col-md-10 mx-auto">
                         <div class="row">
-                            <div class="work-image">
-                                 <img src="img\IMG_3273 2.JPG" width="320" height="320" alt="写真">
-                                 <img src="img\IMG_7805.jpg" width="320" height="296" alt="写真">
+                            <div class="work-image　mx-auto">
+                                <img src="img\IMG_3273 2.JPG" width="320" height="320" alt="写真">
+                                <img src="img\IMG_7805.jpg" width="320" height="296" alt="写真">
                             </div>
                             <div class="work-block　mx-auto">
                                 <div class="work-title">
@@ -96,8 +96,8 @@
                 <div class="row">
                     <div class="headline col-md-10 mx-auto">
                         <div class="row">
-                            <div class="work-image">
-                                 <img src="img\IMG_3342 2.JPG" width="320" height="356" alt="写真">
+                            <div class="work-image　mx-auto">
+                                <img src="img\IMG_3342 2.JPG" width="320" height="356" alt="写真">
                             </div>
                             <div class="work-block　mx-auto">
                                 <div class="work-title">
@@ -116,8 +116,8 @@
                 <div class="row">
                     <div class="headline col-md-10 mx-auto">
                         <div class="row">
-                            <div class="work-image">
-                                 <img src="img\IMG_3344 2.JPG" width="416" height="320" alt="写真">
+                            <div class="work-image　mx-auto">
+                                <img src="img\IMG_3344 2.JPG" width="416" height="320" alt="写真">
                             </div>
                             <div class="work-block　mx-auto">
                                 <div class="work-title">
@@ -136,8 +136,8 @@
                 <div class="row">
                     <div class="headline col-md-10 mx-auto">
                         <div class="row">
-                            <div class="work-image">
-                                 <img src="img\IMG_3346.JPG" width="320" height="356" alt="写真">
+                            <div class="work-image　mx-auto">
+                                <img src="img\IMG_3346.JPG" width="320" height="356" alt="写真">
                             </div>
                             <div class="work-block　mx-auto">
                                 <div class="work-title">
@@ -156,7 +156,7 @@
                 <div class="row">
                     <div class="headline col-md-10 mx-auto">
                         <div class="row">
-                            <div class="work-image">
+                            <div class="work-image　mx-auto">
                                 <img src="img\IMG_4480.jpg" width="424" height="320" alt="写真">
                             </div>
                             <div class="work-block　mx-auto">
@@ -176,7 +176,7 @@
                 <div class="row">
                     <div class="headline col-md-10 mx-auto">
                         <div class="row">
-                            <div class="work-image">
+                            <div class="work-image　mx-auto">
                                 <img src="img\IMG_6394.jpg" width="439" height="320" alt="写真">
                             </div>
                             <div class="work-block　mx-auto">
@@ -196,7 +196,7 @@
                 <div class="row">
                     <div class="headline col-md-10 mx-auto">
                         <div class="row">
-                            <div class="work-image">
+                            <div class="work-image　mx-auto">
                                 <img src="img\IMG_8122.jpg" width="427" height="320" alt="写真">
                             </div>
                             <div class="work-block　mx-auto">
@@ -216,7 +216,7 @@
                 <div class="row">
                     <div class="headline col-md-10 mx-auto">
                         <div class="row">
-                            <div class="work-image">
+                            <div class="work-image　mx-auto">
                                 <img src="img\IMG_9790.JPG" width="390" height="320" alt="写真">
                             </div>
                             <div class="work-block　mx-auto">
