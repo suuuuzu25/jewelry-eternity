f9bf1f16624ef41ac50cb526ab214bd23a674477
おしゅたん <suzu@Airtan.local>
2020-07-07 19:00:57 +0900

    create　.gitignore

--- resources/views/emails/contact_receive.blade.php
+++ resources/views/emails/contact_receive.blade.php
@@ -1,11 +1,8 @@
-
 {{ $contact->name }} 様からお問い合わせがありました。
 
 【お問い合わせ内容】
 {{ $contact->body }}
 
 ______________________
-
-お問い合わせ一覧は　　→https://peaceful-forest-12416.herokuapp.com/admin/contact
-
-管理者トップページは　→https://peaceful-forest-12416.herokuapp.com/admin
+管理者トップページはこちら　→　http://peaceful-forest-12416.herokuapp.com/admin
+お問い合わせ内容の確認はこちらから→ http://peaceful-forest-12416.herokuapp.com/admin/contact
